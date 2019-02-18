c4d6f4696cc9c4016285b0fb117163f446a6e594
Bastian Ike <bastian.ike@aoe.com>
2019-02-18 11:26:02 +0100

    step0

---
+++ go.mod
@@ -0,0 +1,6 @@
+module flamingo.me/example-helloworld
+
+require (
+	flamingo.me/dingo v0.1.3
+	flamingo.me/flamingo/v3 v3.0.0-alpha4
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flamingo.me/dingo"
+	"flamingo.me/flamingo/v3"
+	"flamingo.me/flamingo/v3/core/gotemplate"
+	"flamingo.me/flamingo/v3/core/requestlogger"
+	"flamingo.me/flamingo/v3/core/zap"
+)
+
+// main is our entry point
+func main() {
+	flamingo.App([]dingo.Module{
+		new(zap.Module),           // log formatter
+		new(requestlogger.Module), // requestlogger show request logs
+		new(gotemplate.Module),    // gotemplate installs a go template engine (in debug mode, todo fix this)
+	})
+}
