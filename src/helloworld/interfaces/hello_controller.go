f10af633a134705bf9f06767f5565a84c7612af5
Bastian Ike <bastian.ike@aoe.com>
2019-02-18 11:54:34 +0100

    step3

--- src/helloworld/interfaces/hello_controller.go
+++ src/helloworld/interfaces/hello_controller.go
@@ -6,9 +6,15 @@ import (
 	"flamingo.me/flamingo/v3/framework/web"
 )
 
-type HelloController struct {
-	responder *web.Responder
-}
+type (
+	HelloController struct {
+		responder *web.Responder
+	}
+
+	helloViewData struct {
+		Name string
+	}
+)
 
 func (controller *HelloController) Inject(responder *web.Responder) {
 	controller.responder = responder
@@ -16,5 +22,7 @@ func (controller *HelloController) Inject(responder *web.Responder) {
 
 func (controller *HelloController) Get(ctx context.Context, r *web.Request) web.Result {
 	// Calling the Render method from the response helper and render the template "hello"
-	return controller.responder.Render("hello", nil)
+	return controller.responder.Render("hello", helloViewData{
+		Name: "World",
+	})
 }
